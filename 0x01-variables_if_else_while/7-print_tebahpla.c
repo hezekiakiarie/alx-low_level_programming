098a03cc1c69034d32778bb11b68df6e5cf5b4c2
hezekiakiarie <hezekiarie10@gmail.com>
2022-09-09 03:46:13 -0700

    difficult

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,30 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@hezekiakiarie 
-yuki528
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x01-variables_if_else_while/7-print_tebahpla.c
-@yuki528
-yuki528 Hard work pays
-Latest commit 4cf146d 2 hours ago
- History
- 1 contributor
-25 lines (23 sloc)  369 Bytes
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
