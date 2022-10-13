9e7d1f95d7dc06d7a48762cc82b6a59954c849c4
hezekiakiarie <hezekiarie10@gmail.com>
2022-10-13 03:35:23 -0700

    fixing problem

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#includ "function_pointers.h"
+#include "function_pointers.h"
 
 /**
  * int_index - searches for an integer
@@ -12,26 +12,18 @@
  * return -1. If size <= 0, return -1.
  */
 int int_index(int *array, int size, int (*cmp)(int))
-
 {
-
 	int i;
 
 	if (array && cmp)
-
 	{
-		if(size <= 0)
-
+		if (size <= 0)
 			return (-1);
 
-		for (i=0; i < size; i++)
-
+		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
-
 				return (i);
-
 	}
-
 
 	return (-1);
 }
