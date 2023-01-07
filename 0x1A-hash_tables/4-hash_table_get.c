c6d6c31149f1aec4dc9c6c158241ef4eb506f80a
hezekiakiarie <hezekiarie10@gmail.com>
2023-01-07 04:39:20 -0800

    first

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,28 +1,32 @@
 
 /**
- * hash_table_get - returns that value of the key
- * @ht: hash table to search in.
- * @key: key to find
+ * hash_table_get - retrieves a value associated with a key
  *
- * Return: Value at the key location
+ * @ht: pointer to the hash table
+ * @key: key of the hash
+ * Return: value of the hash.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int idx;
-	hash_node_t *node;
+	unsigned long int k_index;
+	hash_node_t *tmp;
 
-	if (!ht || !key)
+	if (ht == NULL)
 		return (NULL);
-	idx = key_index((unsigned char *)key, ht->size);
-	node = ht->array[idx];
 
-	while (node)
+	if (key == NULL || *key == '\0')
+		return (NULL);
+
+	k_index = key_index((unsigned char *)key, ht->size);
+
+	tmp = ht->array[k_index];
+
+	while (tmp != NULL)
 	{
-		if (!strcmp(key, node->key))
-			return (node->value);
-		node = node->next;
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
 	}
 
 	return (NULL);
 }
-
